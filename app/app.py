5169f318a9449430fc9ec778e0ac7618da7f3ab5
Jhouny <jhonyaraujo9121@gmail.com>
2026-01-12 10:45:23 +0100

    Added CORS header

--- app/app.py
+++ app/app.py
@@ -1,28 +1,29 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from SPARQLWrapper import SPARQLWrapper, JSON, RDFXML, GET
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route('/sparql', methods=['GET'])
 def query_sparql():
-    print("Received SPARQL query request")
+    print("Received SPARQL query request: ", request.args)
     # Récupération de la requête passée en paramètre d'URL
     query = request.args.get('query')
     
     if not query:
         return jsonify({"error": "No query provided"}), 400
 
-    sparql = SPARQLWrapper("http://10.56.62.206:7200/repositories/Gdb-Navig-Cine")
+    sparql = SPARQLWrapper("http://localhost:7200/repositories/Gdb-Navig-Cine")
     sparql.setMethod(GET)
     sparql.setQuery(query)
-    sparql.setReturnFormat(RDFXML)
+    sparql.setReturnFormat(JSON)
 
     try:
-        results = sparql.query()
+        results = sparql.query().convert()
         print("results:", results)
         return jsonify(results)
     except Exception as e:
