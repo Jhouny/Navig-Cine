9a8bbf00047b15c7f8e15bae4567a9047cd479f6
aguyonnaud <agnes.guyonnaud@insa-lyon.fr>
2026-01-13 16:57:11 +0100

    algo reco

---
+++ reco_profile.py
@@ -0,0 +1,63 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pandas as pd
+
+def get_reco(profil):
+
+    genres = profil.get("genres")
+    realisateurs = profil.get("realisateurs")
+    acteurs = profil.get("acteurs")
+
+    string_de_genres = ", ".join(genres.keys())
+    string_de_realisateurs = ", ".join(realisateurs.keys())
+    string_dacteurs = ", ".join(acteurs.keys())
+    
+    sparql = SPARQLWrapper("https://fr.dbpedia.org/sparql")
+    
+    # Requête complexe : Albums, dates et genres
+    query = f"""
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX dbr: <http://dbpedia.org/resource/>
+
+    SELECT ?film 
+    (GROUP_CONCAT(DISTINCT ?director; separator=", ") AS ?directors) 
+    (GROUP_CONCAT(DISTINCT ?genre; separator=", ") AS ?genres) 
+    (GROUP_CONCAT(DISTINCT ?actor; separator=", ") AS ?actors)
+    WHERE {{{{ 
+      ?film dbo:director ?director .
+      FILTER(?director IN({string_de_realisateurs})) 
+    }}UNION{{
+        ?film dbo:starring ?actor.
+        FILTER(?actor IN ({string_dacteurs})) 
+    }}UNION{{
+        ?film dbo:genre ?genre.
+        FILTER ( ?genre IN ({string_de_genres}))
+    }}}}
+    GROUP BY ?film
+    """
+    
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    
+    # Requête et transformation du résultats en pandas dataframe
+    data = []
+    for result in results["results"]["bindings"]:
+        score = 0
+        liste_dacteurs = result.get("actors", {}).get("value", "N/A").split(", ")
+        liste_de_reals = result.get("directors", {}).get("value", "N/A").split(", ")
+        liste_de_genres = result.get("genres", {}).get("value", "N/A").split(", ")
+
+        #todo : calculer score
+        
+        data.append({
+            "Film": result.get("film", {}).get("value", "N/A"),
+            "Realisateurs" : liste_de_reals,#gerer liste
+            "Acteurs": liste_dacteurs,#gerer liste
+            "Genres": liste_de_genres,#gerer liste
+            "Score": score
+        })
+    return pd.DataFrame(data)
+
+# Lancement de la requête
+df_music = query_dbpedia("Interstellar")
+df_music.head()
